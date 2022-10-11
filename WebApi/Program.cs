13f7e2b4115a8ac0c7732287ca5a0dc8fe5e1d07
Mustafa <info@mbulut.com>
2022-10-11 15:58:05 +0300

    Update Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -128,7 +128,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API"); });
 }
 app.ConfigureCustomExceptionMiddleware();
 app.UseCors("AllowOrigin");
