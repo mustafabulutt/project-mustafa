ba499b6a363a1e47415ee96d99aa607933e13176
Mustafa <info@mbulut.com>
2022-10-12 01:05:55 +0300

    ++

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -125,16 +125,21 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API");
-       
-//    });
-    
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+   
 
-//}
+
+}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API");
+
+
+});
+
 app.ConfigureCustomExceptionMiddleware();
 app.UseCors("AllowOrigin");
 app.UseHangfireDashboard();
